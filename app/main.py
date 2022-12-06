4070f12ba300894d1f01cd2f16230e3c00a6c0ff
leonardoalvarez20 <117400391+leonardoalvarez20@users.noreply.github.com>
2022-12-06 01:48:04 -0700

    feat(app): change app port (#8)
    Co-authored-by: Leonardo Alvarez <leonardoalvarez1620@hotmail.com>

--- app/main.py
+++ app/main.py
@@ -38,4 +38,4 @@ fastapi_app.include_router(api_router, prefix="/api/v1")
 app = fastapi_app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
