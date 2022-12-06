e98b0e47c74d8b98ee9cd31b11645d4b3b2cf55c
leonardoalvarez20 <117400391+leonardoalvarez20@users.noreply.github.com>
2022-12-06 01:38:55 -0700

    feat(search): remove token validation (#7)
    Co-authored-by: Leonardo Alvarez <leonardoalvarez1620@hotmail.com>

--- app/api/api_V1/endpoints/search.py
+++ app/api/api_V1/endpoints/search.py
@@ -5,20 +5,12 @@ from fastapi import APIRouter, Depends
 
 from app import schemas
 from app.services.search_service import SearchService
-from app.services.user_service import UserService
 
 router = APIRouter()
-
-user_service_instance = UserService()
-
-
-def validate_token(is_valid=Depends(user_service_instance)):
-    return is_valid
 
 
 @router.get("/searchComics")
 def search_characters_by_query(
-    _is_token_valid: bool = Depends(validate_token),
     query_params: schemas.SearchQueryParams = Depends(),
     search_service: SearchService = Depends(),
 ):
